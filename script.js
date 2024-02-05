953e4269f1eae74c76e72b69bf7b1e143c8ce801
Jehu Amanna <jehu.amanna@niveussolutions.com>
2024-02-05 17:02:02 +0530

    changes

---
+++ script.js
@@ -0,0 +1,88 @@
+const appElement = document.getElementById("app");
+
+let cellClicked = [0,0]
+
+
+const table = []
+
+
+for (let i = 0; i < 10; i++){
+    const row = document.createElement('div');
+    table[i] = []
+    row.classList.add('row-outer')
+    for (let j = 0 ; j < 8; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add('cell-outer')
+        cell.contentEditable = true;
+        cell.addEventListener("click", () => {
+            cellClicked= [i,j];
+            console.log(cellClicked)
+        })
+        table[i].push(cell)
+        row.appendChild(cell);
+        
+    }
+    appElement.append(row);
+}
+
+var ctrlDown = false,
+        ctrlKey = 17,
+        cmdKey = 91,
+        vKey = 86,
+        cKey = 67;
+
+document.addEventListener('keydown', pasteData)
+
+async function pasteData(e) {
+   e.preventDefault();  
+    var key = e.which || e.keyCode; // keyCode detection
+    var ctrl = e.ctrlKey ? e.ctrlKey : ((key === 17) ? true : false); // ctrl detection
+
+    if ( key == 86 && ctrl ) {
+        console.log("Ctrl + V Pressed !");
+        try {
+            const clipboardContents = await navigator.clipboard.read();
+            for (const item of clipboardContents) {
+              for (const mimeType of item.types) {
+                const mimeTypeElement = document.createElement("p");
+                mimeTypeElement.innerText = `MIME type: ${mimeType}`;
+                console.log(mimeTypeElement)
+                if (mimeType === "text/plain") {
+                    const blob = await item.getType("text/plain");
+                    const blobText = await blob.text();
+                  pasteIntoCells(blobText)
+                  console.log(blobText)
+                } else if (mimeType === "text/html") {
+                    const blob = await item.getType("text/html");
+                    const blobText = await blob.text();
+                  pasteIntoCells(blobText)
+                  console.log(blobText)
+                }  
+                else {
+                  throw new Error(`${mimeType} not supported.`);
+                }
+              }
+            }
+          } catch (error) {
+            console.log(error.message);
+          }
+    } else if ( key == 67 && ctrl ) {
+        console.log("Ctrl + C Pressed !");
+    }
+
+    
+  }
+
+
+function pasteIntoCells(data) {
+
+    
+    const massagedData = data.split("\n").map(i => i.split("\t"));
+    console.log(massagedData, cellClicked)
+    
+    for(let i = 0; i< Math.min(10, massagedData.length); i++){
+        for(let j = 0; j < Math.min(8, massagedData[0].length); j++){
+            table[i + cellClicked[0]][j + cellClicked[1]].innerHTML = massagedData[i][j];
+        }
+    }
+}
